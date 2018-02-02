8f6d521347fcf6bbfa8f402ae421e9756270c810
njang <neo.w.jang@gmail.com>
2018-02-01 18:11:45 -0600

    Add simple content for Home and TodosContainer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import TodosContainer from './containers/TodosContainer';
+// import Home from './components/Home';
+// import TodosContainer from './containers/TodosContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World</h1>
-        <Switch>
-          <Route exact path='/' component={ Home }/>
-          <Route path='/todos' component={ TodosContainer }/>
-        </Switch>
       </div>
     );
   }
